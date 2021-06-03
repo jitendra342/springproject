ef13438c5dc555a12123a6911568125d88f7b4e6
jitendra342 <ghanta.jithendra@gmail.com>
2021-06-03 16:11:47 +0530

    created new project

---
+++ springbootnewproject/src/main/java/com/capgemini/springbootnewproject/App.java
@@ -0,0 +1,19 @@
+package com.capgemini.springbootnewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ @Spri*/
+@SpringBootApplication
+public class App 
+{
+    public static void main(String[] args)
+    {
+    	SpringApplication.run(App.class, args);
+    	
+    }
+    
+}
